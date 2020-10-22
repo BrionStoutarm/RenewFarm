f97a1f36484ef37759e203dc2795ee95db1f71e6
BrionStoutarm <macksmith675@gmail.com>
2020-10-22 13:35:29 -0700

    initial commit
    GroundSegment began, has basic mouse over highlighting

---
+++ RenewGame/Assets/GroundSegment.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSegment : MonoBehaviour
+{
+    public Color startColor;
+    public Color highlightColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = highlightColor;
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = startColor;
+    }
+}
