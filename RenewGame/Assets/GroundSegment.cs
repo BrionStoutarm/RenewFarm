55b1eed2a4d36283db1580d024ded9af63579f4b
BrionStoutarm <macksmith675@gmail.com>
2020-10-23 16:21:18 -0700

    Updated ground segment
    Changed from a single cube object to 6 planes.  I think this will make manipulating each side easier.

--- RenewGame/Assets/GroundSegment.cs
+++ RenewGame/Assets/GroundSegment.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class GroundSegment : MonoBehaviour
 {
-    public Color startColor;
+    public Color startTopColor;
+    public Color startSideColor;
     public Color highlightColor;
+
+    GameObject curHoveredSide;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        curHoveredSide = null;
     }
 
     // Update is called once per frame
@@ -21,11 +24,36 @@ public class GroundSegment : MonoBehaviour
 
     private void OnMouseEnter()
     {
-        GetComponent<Renderer>().material.color = highlightColor;
+        Debug.Log("Mouse Enter");
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+        {
+            Debug.Log("Missed");
+            return;
+        }
+
+        GameObject side = hit.collider.gameObject;
+        if(side)
+        {
+            side.GetComponent<Renderer>().material.color = highlightColor;
+            curHoveredSide = side;
+        }
     }
 
     private void OnMouseExit()
     {
-        GetComponent<Renderer>().material.color = startColor;
+        if(curHoveredSide)
+        {
+            if (curHoveredSide.name == "Top")
+            {
+                curHoveredSide.GetComponent<Renderer>().material.color = startTopColor;
+            }
+            else
+                curHoveredSide.GetComponent<Renderer>().material.color = startSideColor;
+        }
+        curHoveredSide = null;
+        Debug.Log("Mouse Exit");
     }
 }
